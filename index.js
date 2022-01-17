54170c7b7deeffb49a0806fbdb68a6c531939a5f
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-17 10:33:36 +0100

    better fetch pushed url's content

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 import 'htmx.org' // htmx from or { htmx } from don't work
 import 'https://unpkg.com/hyperscript.org@0.9.3' // not yet on npm
 
-let APP_PATH = '/app' // eslint-disable-line
+// FIXME: make a proper module with an options object
+let HOME_PATH = '/' // eslint-disable-line
+let APP_ID = 'app' // eslint-disable-line
+let PUSH_SELECTOR = 'main' // eslint-disable-line
+
 let DEV_PROTOCOL = 'http' // eslint-disable-line
 let DEV_ADDRESS = 'localhost' // eslint-disable-line
 let DEV_PORT = 3001 // eslint-disable-line
-let DEV_BASE = `${DEV_PROTOCOL}://${DEV_ADDRESS}:${DEV_PORT}` // eslint-disable-line
+let DEV_ORIGIN = `${DEV_PROTOCOL}://${DEV_ADDRESS}:${DEV_PORT}` // eslint-disable-line
+
 let API_PROTOCOL = 'http' // eslint-disable-line
 let API_ADDRESS = 'localhost' // eslint-disable-line
 let API_PORT = 3000 // eslint-disable-line
-let API_BASE = `${API_PROTOCOL}://${API_ADDRESS}:${API_PORT}` // eslint-disable-line
+let API_ORIGIN = `${API_PROTOCOL}://${API_ADDRESS}:${API_PORT}` // eslint-disable-line
 
+// if served from dev, force ajax urls to the api server
 htmx.on('htmx:configRequest', function ({ detail }) { // eslint-disable-line
-  const { origin, pathname, search } = window.location
-  const DEVELOPMENT = origin === DEV_BASE
-  const url = new URL(detail.path, DEVELOPMENT ? API_BASE : origin)
-  if (detail.path === APP_PATH) {
-    url.searchParams.append('url', encodeURIComponent(`${pathname}${search}`))
-  }
+  const { origin } = window.location
+  const DEVELOPMENT = origin === DEV_ORIGIN
+  const url = new URL(detail.path, DEVELOPMENT ? API_ORIGIN : origin)
   detail.path = url.toString()
+})
+
+// after loading the app, fetch any pushed url's content
+htmx.on('htmx:afterSwap', function ({ detail }) { // eslint-disable-line
+  const { pathname, search } = window.location
+  if (detail.target.id === APP_ID && pathname !== HOME_PATH) {
+    const push = `${pathname}${search}`
+    htmx.ajax('GET', push, PUSH_SELECTOR) // eslint-disable-line
+  }
 })
 
 htmx.on('htmx:beforeSend', function ({ detail }) { // eslint-disable-line
