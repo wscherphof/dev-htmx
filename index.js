b5696ec3f5374436bac810db8b2ae18169363342
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-19 19:37:09 +0100

    bumped _hyperscript

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import htmx from './htmx'
-import 'https://unpkg.com/hyperscript.org@0.9.3' // not yet on npm
+import 'https://unpkg.com/hyperscript.org@0.9.4' // not yet on npm
 
 let INITED
 
-function init(options = {}) {
+function init (options = {}) {
   if (INITED) {
     return
   } else {
     INITED = true
   }
   const defaults = {
-    homePath: '/',
     appId: 'app',
     dev: {
       protocol: 'http',
@@ -26,7 +25,7 @@ function init(options = {}) {
   options.dev = Object.assign(defaults.dev, options.dev)
   options.api = Object.assign(defaults.api, options.api)
   options = Object.assign(defaults, options)
-  const { homePath, appId, dev, api } = options
+  const { appId, dev, api } = options
   // pass either protocol and/or address and/or port, or the complete origin
   dev.origin = dev.origin || `${dev.protocol}://${dev.address}:${dev.port}`
   api.origin = api.origin || `${api.protocol}://${api.address}:${api.port}`
